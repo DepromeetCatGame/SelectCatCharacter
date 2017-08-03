62c65a47974ae2dc710eb364cda190e29c0439b0
annbang <adh0463@naver.com>
2017-08-03 19:45:15 +0900

    고양이 선택 화면
    고양이 선택 화면

---
+++ Assets/CharacterManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CharacterManager : MonoBehaviour {
+    enum indexes { CATIMG_START=0, CATIMG1=1, CATIMG2, CATIMG3, CATIMG_END};
+    private indexes nIndex = indexes.CATIMG1;
+    public string imgName;
+    public int Status = 1;
+
+    public int LeftTurn()
+    {
+        nIndex--;
+        if (nIndex == indexes.CATIMG_START)
+            nIndex = indexes.CATIMG_END-1;
+
+        Status = 1;
+        return (int)nIndex;
+    }
+
+    public int RightTurn()
+    {
+        nIndex++;
+        if (nIndex == indexes.CATIMG_END)
+            nIndex = indexes.CATIMG_START+1;
+
+        Status = 1;
+        return (int)nIndex;
+    }
+
+	// Use this for initialization
+	void Start () {   }
+	
+	// Update is called once per frame
+	void Update () {   }
+    
+    public Texture LoadPNGTexture()
+    {
+        if (imgName == "") imgName = "catimg";
+        string fname = imgName;
+        fname += (int)nIndex;
+        Debug.Log(fname);
+        return Resources.Load(fname) as Texture;
+    }
+    /*
+    public Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f)
+    {
+        Sprite NewSprite = new Sprite();
+        Texture2D SpriteTexture = LoadTexture(FilePath);
+        NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit);
+
+        return NewSprite;
+    }
+    */
+    public Texture2D LoadTexture(string FilePath)
+    {
+        Texture2D Tex2D;
+        byte[] FileData;
+
+        if (File.Exists(FilePath))
+        {
+            FileData = File.ReadAllBytes(FilePath);
+            Tex2D = new Texture2D(2, 2);
+            if (Tex2D.LoadImage(FileData)) 
+                return Tex2D;              
+        }
+        return null;
+    }
+}
---
+++ Assets/CharacterPic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPic : MonoBehaviour {
+    public Texture myTexture;
+    private int x, y, W,H;
+    CharacterManager gmComponent;
+
+    // Use this for initialization
+    void Start () {
+        //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        //Renderer rend = go.GetComponent<Renderer>();
+        //rend.material.mainTexture = Resources.Load("cat-icon") as Texture;
+        gmComponent = GameObject.Find("CharacterManager").GetComponent<CharacterManager>();
+        myTexture = gmComponent.LoadPNGTexture();
+
+        W = Screen.width / 2;
+        H = Screen.height / 2;
+        x = W / 2;
+        y = H / 2;
+        //Resources.Load("cat-icon");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(gmComponent.Status == 1)
+        {
+            gmComponent.Status = 0;
+            myTexture = gmComponent.LoadPNGTexture();
+        }
+	}
+
+    private void OnGUI()
+    {
+        //Debug.Log("test");
+        if (myTexture != null)
+            GUI.DrawTexture(new Rect(x, y, W, H), myTexture);
+        //else
+        //    Debug.Log("cat image is missing");
+    }
+}
